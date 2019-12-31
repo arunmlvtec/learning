4aa1d08ef62d1795ff80713fa850d855ecdbb2d7
arunmlvtec <arun.pokharna@gmail.com>
2019-12-31 03:11:15 -0800

    Added Sum Adds to Zero under functional pacckage

--- src/com/java/learning/Main.java
+++ src/com/java/learning/Main.java
@@ -5,16 +5,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        for(int i = 0; i < 3; i++)
-            for(int j = 0; j < 3; j++) {
-                for(int k = 0; k < 3; k++) {
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
                    /* if (i == j || i == k || j == k)
                         continue;
                     else*/
-                        System.out.println(i+""+j+""+k);
+                    System.out.println(i + "" + j + "" + k);
                 }
             }
+        }
     }
-
-
 }
--- src/com/java/learning/functional/AddsToZero.java
+++ src/com/java/learning/functional/AddsToZero.java
@@ -1,8 +1,41 @@
 package com.java.learning.functional;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddsToZero {
     public void printNumsThatAddsToZero(int[] inputArray) {
+        List<ArrayList<Integer>> distinctPairs = new ArrayList<>();
         int totalPairs = 0;
+        for(int i = 0; i < inputArray.length; i++) {
+            for (int j = i + 1; j < inputArray.length - 1; j++) {
+                for (int k = j + 1; k < inputArray.length - 2; k++) {
+                    if ((inputArray[i] + inputArray[j] + inputArray[k]) == 0) {
+                        totalPairs += 1;
+                        distinctPairs.add(new ArrayList<>(Arrays.asList(inputArray[i], inputArray[j], inputArray[k])));
+                    }
+                }
+            }
+        }
+        System.out.println("Total Distinct pairs are: "+totalPairs);
+        for(ArrayList<Integer> values: distinctPairs) {
+            for(Integer value: values) {
+                System.out.print(value+"\t");
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        AddsToZero addsToZero = new AddsToZero();
+        Scanner scanner = new Scanner(System.in);
+        int arrayLength = scanner.nextInt();
+        int[] inputArray = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            inputArray[i] = scanner.nextInt();
+        }
+        addsToZero.printNumsThatAddsToZero(inputArray);
     }
 }
