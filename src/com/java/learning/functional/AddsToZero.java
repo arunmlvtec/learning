3d060fd66d5b40132a474c9a3733a6f1f8058c75
arunmlvtec <arun.pokharna@gmail.com>
2019-12-31 02:32:10 -0800

    Updated TwoDArray program

---
+++ src/com/java/learning/functional/AddsToZero.java
@@ -0,0 +1,8 @@
+package com.java.learning.functional;
+
+public class AddsToZero {
+    public void printNumsThatAddsToZero(int[] inputArray) {
+        int totalPairs = 0;
+
+    }
+}
--- src/com/java/learning/functional/TwoDArray.java
+++ src/com/java/learning/functional/TwoDArray.java
@@ -6,12 +6,14 @@ public class TwoDArray<T> {
     public void printArray(T [][] array) {
         for (T[] anArray : array) {
             for (T anAnArray : anArray) {
-                System.out.println(anAnArray);
+                System.out.print(anAnArray+"\t");
             }
+            System.out.println();
         }
     }
 
-    public void populate2DArray() {
+    public static void main(String[] args) {
+        TwoDArray twoDArray = new TwoDArray();
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number of rows");
         int rows = scan.nextInt();
@@ -19,6 +21,37 @@ public class TwoDArray<T> {
         int cols = scan.nextInt();
         System.out.println("Enter Elements of this array");
         Scanner inputArrayScan = new Scanner(System.in);
+        if (inputArrayScan.hasNextBoolean()) {
+            Boolean[][] inputArray = new Boolean[rows][cols];
+            for (int i = 0; i < inputArray.length; i++) {
+                for (int j = 0; j < inputArray[i].length; j++) {
+                    inputArray[i][j] = inputArrayScan.nextBoolean();
+                }
+            }
+            twoDArray.printArray(inputArray);
+        }
+        else if (inputArrayScan.hasNextInt()) {
+            Integer[][] inputArray = new Integer[rows][cols];
+            for (int i = 0; i < inputArray.length; i++) {
+                for (int j = 0; j < inputArray[i].length; j++) {
+                    inputArray[i][j] = inputArrayScan.nextInt();
+                }
+            }
 
+            twoDArray.printArray(inputArray);
+        }
+        else if (inputArrayScan.hasNextDouble()) {
+            Double[][] inputArray = new Double[rows][cols];
+            for (int i = 0; i < inputArray.length; i++) {
+                for (int j = 0; j < inputArray[i].length; j++) {
+                    inputArray[i][j] = inputArrayScan.nextDouble();
+                }
+            }
+
+            twoDArray.printArray(inputArray);
+        }
+        else {
+            System.out.println("Invalid input!");
+        }
     }
 }
